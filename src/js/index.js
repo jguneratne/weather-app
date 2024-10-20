3b60c798c71458c4e28d7d6241e9676bfc755804
jguneratne <59374962+jguneratne@users.noreply.github.com>
2024-10-20 09:25:05 -0400

    Limit historic forecast date picker to today's date

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,14 @@
 console.log("Weather App");
 
+import { today, date2Input } from "./variables";
+
 import { selectForecastTab } from "./tabs-ctrl";
 
 import { getTodayInput, getHistoricInput } from "./user-input";
 
 document.addEventListener("DOMContentLoaded", () => {
+  date2Input.setAttribute("max", today);
+
   selectForecastTab();
   getTodayInput();
   getHistoricInput();
