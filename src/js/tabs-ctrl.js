36ad5e2d6d0b5023ef943aab9ea71edcf5980869
jguneratne <59374962+jguneratne@users.noreply.github.com>
2024-10-21 17:48:45 -0400

    Reorganize html and rework tabs-ctrl.js

--- src/js/tabs-ctrl.js
+++ src/js/tabs-ctrl.js
@@ -1,29 +1,33 @@
 import {
   tabBtns,
-  selectToday,
-  selectHistoric,
+  datesDiv,
   date1Input,
-  todayMinusFifteen,
   date2Input,
   today,
+  todayMinusFifteen,
+  forecastDivs,
   todayForecastDiv,
-  historicForecastDiv,
 } from "./variables";
 
 export function selectForecastTab() {
-  todayForecastDiv.classList.add("form-div--active");
+  todayForecastDiv.classList.add("forecast-info--active");
 
   tabBtns.forEach((btn) => {
     btn.addEventListener("pointerdown", (e) => {
-      if (e.target === selectToday) {
-        todayForecastDiv.classList.add("form-div--active");
-        historicForecastDiv.classList.remove("form-div--active");
-      } else if (e.target === selectHistoric) {
-        todayForecastDiv.classList.remove("form-div--active");
-        historicForecastDiv.classList.add("form-div--active");
-        date1Input.value = todayMinusFifteen;
-        date2Input.value = today;
-      }
+      forecastDivs.forEach((div) => {
+        div.classList.remove("forecast-info--active");
+        if (btn.dataset.tab === div.dataset.name) {
+          div.classList.add("forecast-info--active");
+        }
+
+        if (btn.dataset.tab === "historic") {
+          date1Input.value = todayMinusFifteen;
+          date2Input.value = today;
+          datesDiv.style.display = "initial";
+        } else {
+          datesDiv.style.display = "none";
+        }
+      });
     });
   });
 }
--- src/js/variables.js
+++ src/js/variables.js
@@ -1,16 +1,13 @@
 // Tabls Ctrl Variables
 
 export const tabBtns = document.querySelectorAll(".tab-btn");
-export const selectToday = document.querySelector(".today-btn");
-export const selectHistoric = document.querySelector(".historic-btn");
-export const todayForecastDiv = document.querySelector("[data-form='today']");
-export const historicForecastDiv = document.querySelector(
-  "[data-form='historic']"
-);
+export const datesDiv = document.querySelector(".dates-div");
+export const forecastDivs = document.querySelectorAll(".forecast-info");
+export const todayForecastDiv = document.querySelector("[data-name='today']");
 
 // Forecast Data Variables
 
-export const key = "FYZ4WWVAF3DKLSGYMBZA734P4";
+// export const key = "FYZ4WWVAF3DKLSGYMBZA734P4";
 
 export const today = new Date().toISOString().substring(0, 10);
 
@@ -22,112 +19,112 @@ const pastDate = new Date();
 pastDate.setDate(new Date().getDate() - 15);
 export const todayMinusFifteen = pastDate.toISOString().substring(0, 10);
 
-export const todayForm = document.querySelector(".today-form");
-export const historicForm = document.querySelector(".historic-form");
-export const todayLocationInput = document.querySelector(".today-location");
-export const historicLocationInput =
-  document.querySelector(".historic-location");
+// export const todayForm = document.querySelector(".today-form");
+// export const historicForm = document.querySelector(".historic-form");
+// export const todayLocationInput = document.querySelector(".today-location");
+// export const historicLocationInput =
+//   document.querySelector(".historic-location");
 export const date1Input = document.querySelector(".start");
 export const date2Input = document.querySelector(".end");
-export const todayUnitsInput = document.getElementsByName("today-units");
-export const historicUnitsInput = document.getElementsByName("historic-units");
+// export const todayUnitsInput = document.getElementsByName("today-units");
+// export const historicUnitsInput = document.getElementsByName("historic-units");
 
-export let todayLocation = {
-  userTodayLocation: "",
+// export let todayLocation = {
+//   userTodayLocation: "",
 
-  get enteredTodayLocation() {
-    return this.userTodayLocation;
-  },
+//   get enteredTodayLocation() {
+//     return this.userTodayLocation;
+//   },
 
-  set enteredTodayLocation(newUserTodayLocation) {
-    this.userTodayLocation = newUserTodayLocation;
-  },
-};
+//   set enteredTodayLocation(newUserTodayLocation) {
+//     this.userTodayLocation = newUserTodayLocation;
+//   },
+// };
 
-export let historicLocation = {
-  userHistoricLocation: "",
+// export let historicLocation = {
+//   userHistoricLocation: "",
 
-  get enteredHistoricLocation() {
-    return this.userHistoricLocation;
-  },
+//   get enteredHistoricLocation() {
+//     return this.userHistoricLocation;
+//   },
 
-  set enteredHistoricLocation(newUserHistoricLocation) {
-    this.userHistoricLocation = newUserHistoricLocation;
-  },
-};
+//   set enteredHistoricLocation(newUserHistoricLocation) {
+//     this.userHistoricLocation = newUserHistoricLocation;
+//   },
+// };
 
-export let date1 = {
-  userDate1: "",
+// export let date1 = {
+//   userDate1: "",
 
-  get enteredDate1() {
-    return this.userDate1;
-  },
+//   get enteredDate1() {
+//     return this.userDate1;
+//   },
 
-  set enteredDate1(newUserDate1) {
-    this.userDate1 = newUserDate1;
-  },
-};
+//   set enteredDate1(newUserDate1) {
+//     this.userDate1 = newUserDate1;
+//   },
+// };
 
-export let date2 = {
-  userDate2: "",
+// export let date2 = {
+//   userDate2: "",
 
-  get enteredDate2() {
-    return this.userDate2;
-  },
+//   get enteredDate2() {
+//     return this.userDate2;
+//   },
 
-  set enteredDate2(newUserDate2) {
-    this.userDate2 = newUserDate2;
-  },
-};
+//   set enteredDate2(newUserDate2) {
+//     this.userDate2 = newUserDate2;
+//   },
+// };
 
-export let todayUnitGroup = {
-  userTodayUnitGroup: "",
+// export let todayUnitGroup = {
+//   userTodayUnitGroup: "",
 
-  get enteredTodayUnitGroup() {
-    return this.userTodayUnitGroup;
-  },
+//   get enteredTodayUnitGroup() {
+//     return this.userTodayUnitGroup;
+//   },
 
-  set enteredTodayUnitGroup(newTodayUserUnitGroup) {
-    this.userTodayUnitGroup = newTodayUserUnitGroup;
-  },
-};
+//   set enteredTodayUnitGroup(newTodayUserUnitGroup) {
+//     this.userTodayUnitGroup = newTodayUserUnitGroup;
+//   },
+// };
 
-export let historicUnitGroup = {
-  userHistoricUnitGroup: "",
+// export let historicUnitGroup = {
+//   userHistoricUnitGroup: "",
 
-  get enteredHistoricUnitGroup() {
-    return this.userHistoricUnitGroup;
-  },
+//   get enteredHistoricUnitGroup() {
+//     return this.userHistoricUnitGroup;
+//   },
 
-  set enteredHistoricUnitGroup(newHistoricUserUnitGroup) {
-    this.userHistoricUnitGroup = newHistoricUserUnitGroup;
-  },
-};
+//   set enteredHistoricUnitGroup(newHistoricUserUnitGroup) {
+//     this.userHistoricUnitGroup = newHistoricUserUnitGroup;
+//   },
+// };
 
-// Location Data Variables
+// // Location Data Variables
 
-export const geoKey = "cd840e39cf3b4160a25abcf0554fdc0c";
+// export const geoKey = "cd840e39cf3b4160a25abcf0554fdc0c";
 
-export let forecastLatitude = {
-  latitude: "",
+// export let forecastLatitude = {
+//   latitude: "",
 
-  get userLatitude() {
-    return this.latitude;
-  },
+//   get userLatitude() {
+//     return this.latitude;
+//   },
 
-  set userLatitude(newLatitude) {
-    this.latitude = newLatitude;
-  },
-};
+//   set userLatitude(newLatitude) {
+//     this.latitude = newLatitude;
+//   },
+// };
 
-export let forecastLongitude = {
-  longitude: "",
+// export let forecastLongitude = {
+//   longitude: "",
 
-  get userLongitude() {
-    return this.longitude;
-  },
+//   get userLongitude() {
+//     return this.longitude;
+//   },
 
-  set userLongitude(newLongitude) {
-    this.longitude = newLongitude;
-  },
-};
+//   set userLongitude(newLongitude) {
+//     this.longitude = newLongitude;
+//   },
+// };
