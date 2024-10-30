d5d079ae1a4e660932792314e3e89254d06f1792
jguneratne <59374962+jguneratne@users.noreply.github.com>
2024-10-29 22:20:55 -0400

    Adjust user input handling for dynamic menu

--- src/js/user-input.js
+++ src/js/user-input.js
@@ -1,48 +1,93 @@
 import {
+  form,
   locationInput,
   location,
   unitsInput,
   unitGroup,
+  locationsMenu,
   date1Input,
   date1,
   date2Input,
   date2,
   todayBtn,
   historicBtn,
+  todayForecastDiv,
+  fifteenForecastDiv,
+  historicForecastDiv,
 } from "./variables";
 
 import { getTodayForecast, getHistoricForecast } from "./forecast-data";
 
-export function getTodayInput() {
+export function handleFormSubmit() {
   todayBtn.addEventListener("pointerdown", (e) => {
     e.preventDefault();
-    console.log("CLICK!");
+    fetchForecast();
+  });
 
-    location.enteredLocation = locationInput.value;
-
-    for (let unit of unitsInput) {
-      if (unit.checked) {
-        unitGroup.enteredUnitGroup = unit.value;
-      }
-    }
-    getTodayForecast();
+  historicBtn.addEventListener("pointerdown", (e) => {
+    e.preventDefault();
+    fetchForecast();
   });
 }
 
-export function getHistoricInput() {
-  historicBtn.addEventListener("pointerdown", (e) => {
-    e.preventDefault();
-
-    location.enteredLocation = locationInput.value;
-    date1.enteredDate1 = date1Input.value;
-    date2.enteredDate2 = date2Input.value;
-
-    for (let unit of unitsInput) {
-      if (unit.checked) {
-        unitGroup.enteredUnitGroup = unit.value;
-      }
-    }
-
+export function fetchForecast() {
+  if (
+    todayForecastDiv.classList.contains("forecast-info--active") ||
+    fifteenForecastDiv.classList.contains("forecast-info--active")
+  ) {
+    console.log("Loading Today...");
+    handleTodayInput();
+    getTodayForecast();
+    clearLocationField();
+  } else if (historicForecastDiv.classList.contains("forecast-info--active")) {
+    console.log("Loading Historic...");
+    handleHistoricInput();
     getHistoricForecast();
-  });
+    clearLocationField();
+  }
+}
+
+function handleTodayInput() {
+  if (location.value !== "") {
+    location.enteredLocation = locationInput.value;
+  } else {
+    location.enteredLocation = locationsMenu.value;
+    console.log(location.userLocation);
+  }
+
+  for (let unit of unitsInput) {
+    if (unit.checked) {
+      unitGroup.enteredUnitGroup = unit.value;
+    }
+  }
+
+  // console.log(location.userLocation);
+  // console.log(unitGroup.userUnitGroup);
+}
+
+function handleHistoricInput() {
+  if (location.value !== "") {
+    location.enteredLocation = locationInput.value;
+  } else {
+    location.enteredLocation = locationsMenu.value;
+    console.log(location.userLocation);
+  }
+
+  date1.enteredDate1 = date1Input.value;
+  date2.enteredDate2 = date2Input.value;
+
+  for (let unit of unitsInput) {
+    if (unit.checked) {
+      unitGroup.enteredUnitGroup = unit.value;
+    }
+  }
+
+  // console.log(location.userLocation);
+  // console.log(date1.userDate1);
+  // console.log(date2.userDate2);
+  // console.log(unitGroup.userUnitGroup);
+}
+
+function clearLocationField() {
+  locationInput.value = "";
 }
