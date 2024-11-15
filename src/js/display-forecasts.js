551a4a76d90e3ea74a772b7ba560dd47b227e9b8
jguneratne <59374962+jguneratne@users.noreply.github.com>
2024-11-14 19:17:52 -0500

    Add functions to display 15-day and historic forecasts

--- src/js/display-forecasts.js
+++ src/js/display-forecasts.js
@@ -1,4 +1,9 @@
-import { today, todayOnScreen } from "./variables";
+import {
+  today,
+  todayOnScreen,
+  fifteenForecastDiv,
+  historicForecastDiv,
+} from "./variables";
 
 import { weatherIcons } from "./weather-icons";
 
@@ -18,13 +23,13 @@ export function displayToday(todayData) {
 
   todayDescrip.textContent = todayData.days[0].description;
 
-  const iconName = todayData.days[0].icon;
-  const iconURL = weatherIcons[iconName];
-
   // remove old icon
   if (todayIconDiv.hasChildNodes()) {
     todayIconDiv.firstChild.remove();
   }
+
+  const iconName = todayData.days[0].icon;
+  const iconURL = weatherIcons[iconName];
 
   // add new icon
   if (iconURL) {
@@ -42,4 +47,164 @@ export function displayToday(todayData) {
   todayMin.textContent = todayData.days[0].tempmin + "°";
 
   todayMax.textContent = todayData.days[0].tempmax + "°";
+}
+
+export function displayFifteenDay(todayData) {
+  removeFifteen();
+  const fifteenDay = todayData.days;
+  console.log(fifteenDay);
+  fifteenDay.forEach((day) => {
+    const dateLine = document.createElement("div");
+    dateLine.classList = "data-line date-line";
+
+    const date = document.createElement("p");
+    date.classList = "date";
+    date.style.fontWeight = "bold";
+
+    const iconDiv = document.createElement("div");
+    iconDiv.classList = "data-line icon-div";
+
+    const minMaxDiv = document.createElement("div");
+    minMaxDiv.classList = "min-max-div";
+
+    const minLine = document.createElement("div");
+    minLine.classList = "data-line min-line";
+
+    const minHeading = document.createElement("p");
+    minHeading.classList = "data-heading min-heading";
+    minHeading.textContent = "Min:";
+
+    const minTemp = document.createElement("p");
+    minTemp.classList = "min";
+
+    const maxLine = document.createElement("div");
+    maxLine.classList = "data-line max-line";
+
+    const maxHeading = document.createElement("p");
+    maxHeading.classList = "data-heading max-heading";
+    maxHeading.textContent = "Max:";
+
+    const maxTemp = document.createElement("p");
+    maxTemp.classList = "max";
+
+    const divider = document.createElement("p");
+    divider.classList = "divider";
+    divider.textContent = "___________________";
+
+    const dateContent = new Date(day.datetime).toString().slice(0, 15);
+    console.log(dateContent);
+    date.textContent = dateContent;
+
+    const fifteenIconName = day.icon;
+    const fifteenIconURL = weatherIcons[fifteenIconName];
+
+    if (fifteenIconURL) {
+      const fifteenWeatherIcon = document.createElement("img");
+      fifteenWeatherIcon.src = fifteenIconURL;
+      fifteenWeatherIcon.classList.add("weather-icon");
+      fifteenWeatherIcon.setAttribute("alt", fifteenIconName + "icon");
+      iconDiv.appendChild(fifteenWeatherIcon);
+    }
+
+    minTemp.textContent = day.tempmin;
+    maxTemp.textContent = day.tempmax;
+
+    fifteenForecastDiv.appendChild(dateLine);
+    dateLine.appendChild(date);
+    fifteenForecastDiv.appendChild(iconDiv);
+    fifteenForecastDiv.appendChild(minMaxDiv);
+    minMaxDiv.appendChild(minLine);
+    minLine.appendChild(minHeading);
+    minLine.appendChild(minTemp);
+    minMaxDiv.appendChild(maxLine);
+    maxLine.appendChild(maxHeading);
+    maxLine.appendChild(maxTemp);
+    fifteenForecastDiv.appendChild(divider);
+  });
+}
+
+export function displayHistoricDays(historicData) {
+  removeHistoric();
+
+  const historicDays = historicData.days;
+  historicDays.forEach((day) => {
+    const dateLine = document.createElement("div");
+    dateLine.classList = "data-line date-line";
+
+    const date = document.createElement("p");
+    date.classList = "date";
+    date.style.fontWeight = "bold";
+
+    const iconDiv = document.createElement("div");
+    iconDiv.classList = "data-line icon-div";
+
+    const minMaxDiv = document.createElement("div");
+    minMaxDiv.classList = "min-max-div";
+
+    const minLine = document.createElement("div");
+    minLine.classList = "data-line min-line";
+
+    const minHeading = document.createElement("p");
+    minHeading.classList = "data-heading min-heading";
+    minHeading.textContent = "Min:";
+
+    const minTemp = document.createElement("p");
+    minTemp.classList = "min";
+
+    const maxLine = document.createElement("div");
+    maxLine.classList = "data-line max-line";
+
+    const maxHeading = document.createElement("p");
+    maxHeading.classList = "data-heading max-heading";
+    maxHeading.textContent = "Max:";
+
+    const maxTemp = document.createElement("p");
+    maxTemp.classList = "max";
+
+    const divider = document.createElement("p");
+    divider.classList = "divider";
+    divider.textContent = "___________________";
+
+    const dateContent = new Date(day.datetime).toString().slice(0, 15);
+    console.log(dateContent);
+    date.textContent = dateContent;
+
+    const historicIconName = day.icon;
+    const historicIconURL = weatherIcons[historicIconName];
+
+    if (historicIconURL) {
+      const historicWeatherIcon = document.createElement("img");
+      historicWeatherIcon.src = historicIconURL;
+      historicWeatherIcon.classList.add("weather-icon");
+      historicWeatherIcon.setAttribute("alt", historicIconName + "icon");
+      iconDiv.appendChild(historicWeatherIcon);
+    }
+
+    minTemp.textContent = day.tempmin;
+    maxTemp.textContent = day.tempmax;
+
+    historicForecastDiv.appendChild(dateLine);
+    dateLine.appendChild(date);
+    historicForecastDiv.appendChild(iconDiv);
+    historicForecastDiv.appendChild(minMaxDiv);
+    minMaxDiv.appendChild(minLine);
+    minLine.appendChild(minHeading);
+    minLine.appendChild(minTemp);
+    minMaxDiv.appendChild(maxLine);
+    maxLine.appendChild(maxHeading);
+    maxLine.appendChild(maxTemp);
+    historicForecastDiv.appendChild(divider);
+  });
+}
+
+function removeFifteen() {
+  while (fifteenForecastDiv.firstChild) {
+    fifteenForecastDiv.removeChild(fifteenForecastDiv.firstChild);
+  }
+}
+
+function removeHistoric() {
+  while (historicForecastDiv.firstChild) {
+    historicForecastDiv.removeChild(historicForecastDiv.firstChild);
+  }
 }
