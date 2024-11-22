b926b8e182e917de2497466b7056cda6ad90a740
jguneratne <59374962+jguneratne@users.noreply.github.com>
2024-11-22 16:38:57 -0500

    Change order of locations array if duplicate location is searched

--- src/js/location.js
+++ src/js/location.js
@@ -42,16 +42,27 @@ export async function getLocationName() {
       locationData.features[0].properties.country;
 
     if (locationValues.includes(displayLocation.returnedLocation)) {
-      return;
-    } else {
-      // create addLocations variable to retrieve current locationValues and append newest location to older locations
-      const addLocations = JSON.parse(localStorage.getItem("locationValues"));
-      addLocations.push(displayLocation.returnedLocation);
+      // create removeLocation variable to retrieve duplicate entered location and remove from locationValues
+      const removeLocation = JSON.parse(localStorage.getItem("locationValues"));
+      removeLocation.splice(
+        removeLocation.indexOf(displayLocation.returnedLocation),
+        1,
+      );
+      // re-add duplicate location to end of locationValues array
+      removeLocation.push(displayLocation.returnedLocation);
 
-      // set the addLocations variable to locationsValue so old and new locations are in one array
-      localStorage.setItem("locationValues", JSON.stringify(addLocations));
+      localStorage.setItem("locationValues", JSON.stringify(removeLocation));
       console.log(JSON.parse(localStorage.getItem("locationValues")));
-
+      // rebuild location select menu to reflect new order of locations
+      handleLocationDropdown();
+    } else {
+      // create addLocation variable to retrieve current locationValues and append newest location to older locations
+      const addLocation = JSON.parse(localStorage.getItem("locationValues"));
+      addLocation.push(displayLocation.returnedLocation);
+      // set the addLocations variable to locationsValue so old and new locations are in one array
+      localStorage.setItem("locationValues", JSON.stringify(addLocation));
+      console.log(JSON.parse(localStorage.getItem("locationValues")));
+      // rebuild location select menu to display all locations
       handleLocationDropdown();
     }
   } catch (e) {
