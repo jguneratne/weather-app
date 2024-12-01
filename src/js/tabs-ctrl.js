1376fd78406f1499e41ddc1cffc778037359555d
jguneratne <59374962+jguneratne@users.noreply.github.com>
2024-12-01 11:12:52 -0500

    Rework selectForecatTab to control color of tabs

--- src/js/tabs-ctrl.js
+++ src/js/tabs-ctrl.js
@@ -1,5 +1,7 @@
 import {
+  tabBtnsDiv,
   tabBtns,
+  todayTabBtn,
   datesDiv,
   date1Input,
   date2Input,
@@ -10,24 +12,30 @@ import {
 } from "./variables";
 
 export function selectForecastTab() {
+  todayTabBtn.classList.add("tab-btn--active");
   todayForecastDiv.classList.add("forecast-info--active");
 
-  tabBtns.forEach((btn) => {
-    btn.addEventListener("pointerdown", (e) => {
-      forecastDivs.forEach((div) => {
-        div.classList.remove("forecast-info--active");
-        if (btn.dataset.tab === div.dataset.name) {
-          div.classList.add("forecast-info--active");
-        }
+  tabBtnsDiv.addEventListener("pointerdown", (e) => {
+    console.log(e.target.dataset.tab);
 
-        if (btn.dataset.tab === "historic") {
-          date1Input.value = todayMinusNine;
-          date2Input.value = today;
-          datesDiv.style.display = "initial";
-        } else {
-          datesDiv.style.display = "none";
-        }
-      });
+    tabBtns.forEach((btn) => {
+      btn.classList.remove("tab-btn--active");
+    });
+
+    forecastDivs.forEach((div) => {
+      div.classList.remove("forecast-info--active");
+      if (div.dataset.name === e.target.dataset.tab) {
+        div.classList.add("forecast-info--active");
+        e.target.classList.add("tab-btn--active");
+      }
+
+      if (e.target.dataset.tab === "historic") {
+        date1Input.value = todayMinusNine;
+        date2Input.value = today;
+        datesDiv.style.display = "initial";
+      } else {
+        datesDiv.style.display = "none";
+      }
     });
   });
 }
